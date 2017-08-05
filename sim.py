9fcd76cec189e7fa316cdb618f31d5153d9c6e48
Falcon94 <f_jahjah@hotmail.com>
2017-08-06 02:29:09 +0300

    Added Valentina and Michael.

--- sim.py
+++ sim.py
@@ -1,6 +1,6 @@
-
 def ceil(value, interval):
     return value + (interval - value) % interval
+
 
 # PD's skill
 celebrate = [1., 1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5, 1.55]
@@ -8,6 +8,15 @@ celebrateDuration = 8000
 
 # Cupid's Skill
 arrow = [0, 10, 15, 20, 25, 30, 35, 40, 45, 55, 65]
+
+# Mike's Skill
+divinity = [1., 1.18, 1.22, 1.26, 1.3, 1.34, 1.38, 1.43, 1.48, 1.54, 1.6]
+divinityDuration = 8000
+
+# Valentina's Skill
+cyclone = [0., 24., 32., 40., 50., 60., 70., 84., 96., 108., 126.]
+cycloneDuration = [1., 2000, 2000, 2000, 2500, 2500, 2500, 3000, 3000, 3000, 3500]
+cyclonePerTick = [100 * cyclone[i] / cycloneDuration[i] for i in range(0, 10)]
 
 # Talents and Crests
 zerk = [1., 1.1, 1.15, 1.2, 1.25, 1.3, 1.4, 1.5, 1.7]
@@ -19,6 +28,7 @@ blitz = [1., 1.1, 1.15, 1.20, 1.25, 1.3]
 # Enchantments
 fury = [1., 1.1, 1.14, 1.19, 1.24, 1.3]
 
+
 class Hero(object):
     def __init__(self, name, atkSpeed, skillLevel):
         self.name = name
@@ -26,6 +36,8 @@ class Hero(object):
         self.skillLevel = skillLevel
         self.hasAttacked = False
         self.celebrateStacks = []
+        self.divinityStacks = []
+        self.cycloneStacks = []
         self.zerkLevel = 0
         self.lastAttack = 0
         self.lastProc = -100000
@@ -66,15 +78,25 @@ class Hero(object):
     def getAtkSpeed(self):
         tempSpeed = self.atkSpeed
 
-        # Celebrate
+        # PD's celebrate
         for stack in self.celebrateStacks:
             tempSpeed = tempSpeed / celebrate[stack[0]]
+
+        # Mike's divinity
+        for stack in self.divinityStacks:
+            tempSpeed = tempSpeed / divinity[stack[0]]
 
         totalBuff = zerk[self.zerkLevel] * blitz[self.blitzLevel] * fury[self.furyLevel]
         return ceil(tempSpeed / totalBuff, 200)
 
     def addCelebrate(self, celebrateLevel, time):
         self.celebrateStacks.append([celebrateLevel, time])
+
+    def addDivinity(self, skillLevel, time):
+        self.divinityStacks.append([skillLevel, time])
+
+    def addCyclone(self, skillLevel, time, duration):
+        self.cycloneStacks.append([skillLevel, time, duration])
 
     def addEnergy(self, energy):
         self.energy = min(100, round(self.energy + energy, 2))
@@ -84,10 +106,25 @@ class Hero(object):
             if self.lastAttack + self.getAtkSpeed() <= time:
                 self.hasAttacked = False
 
+        # Apply Love Cyclone
+        if time % 100 == 0:
+            for stack in self.cycloneStacks:
+                self.addEnergy(cyclonePerTick[stack[0]])
+
         # Expire celebrate
-        if len(self.celebrateStacks)>0:
-            if self.celebrateStacks[0][1]<= time-8000:
+        if len(self.celebrateStacks) > 0:
+            if self.celebrateStacks[0][1] <= time - 8000:
                 self.celebrateStacks.pop(0)
+
+        # Expire Divinity
+        if len(self.divinityStacks) > 0:
+            if self.divinityStacks[0][1] <= time - divinityDuration:
+                self.divinityStacks.pop(0)
+
+        # Expire Love Cyclone
+        if len(self.cycloneStacks) > 0:
+            if self.cycloneStacks[0][1] <= (time - self.cycloneStacks[0][2]):
+                self.cycloneStacks.pop(0)
 
         if self.hasAttacked == False:
             self.hasAttacked = True
@@ -104,11 +141,17 @@ class Hero(object):
         print(self.name, "proccing")
         if self.name[:2] == "PD":
             for hero in heroArray:
-                hero.addCelebrate(self.skillLevel,time)
+                hero.addCelebrate(self.skillLevel, time)
         if self.name[:5] == "Cupid":
             for hero in heroArray:
                 if hero.name[:5] != "Cupid":
                     hero.addEnergy(arrow[self.skillLevel])
+        if self.name[:3] == "Mike":
+            for hero in heroArray:
+                hero.addDivinity(self.skillLevel, time)
+        if self.name[:3] == "Val":
+            for hero in heroArray:
+                hero.addCyclone(self.skillLevel, time, cycloneDuration[self.skillLevel])
 
 
 # Create your heroes here, following the format:
@@ -124,6 +167,8 @@ class Hero(object):
 #  A few heroes must have a name that starts with a specific prefix:
 #   Pumpkin Duke must start with "PD".
 #   Cupid must start with "Cupid".
+#   Michael must start with "Mike".
+#   Valentina must start with "Val".
 
 pd = Hero("PD", 1000, 10)
 pd.setZerkLevel(8)
@@ -131,9 +176,20 @@ pd.setRevitLevel(5)
 # pd.setBlitzLevel(5)
 # pd.setFuryLevel(5)
 
-# After you create your heroes, add the variable names to this array, then hit
-# the "run" button above
-heroArray = [pd]
+cupid = Hero("Cupid", 1200, 10)
+cupid.setProcCD(6000)
+cupid.setRevitLevel(5)
+
+val = Hero("Val", 1200, 7)
+val.setProcCD(6000)
+# val.setRevitLevel(4)
+
+mike = Hero("Mike", 1500, 9)
+mike.setProcCD(8000)
+# mike.setRevitLevel(5)
+
+# After you create your heroes, add the variable names to this array, then run the program
+heroArray = [pd, val]
 
 # Output shows the time in 100ms increments, it shows the energy *before* the
 # action taken (i.e. PD1 swing 0 800.0 0) means it had 0 energy before that
