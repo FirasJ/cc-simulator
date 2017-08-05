f382875081a385046f6f615b5ac1dfb45519ba9e
Falcon94 <f_jahjah@hotmail.com>
2017-08-06 02:09:14 +0300

    Refactoring

--- sim.py
+++ sim.py
@@ -2,71 +2,87 @@
 def ceil(value, interval):
     return value + (interval - value) % interval
 
+# PD's skill
 celebrate = [1., 1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5, 1.55]
-arrow = [0, 10, 15, 20, 25, 30, 35, 40, 45, 55, 65]
 celebrateDuration = 8000
-zerk = [1., 1.1, 1.15, 1.2, 1.25, 1.3]
-revit = [0,20, 40, 60, 80, 100]
+
+# Cupid's Skill
+arrow = [0, 10, 15, 20, 25, 30, 35, 40, 45, 55, 65]
+
+# Talents and Crests
+zerk = [1., 1.1, 1.15, 1.2, 1.25, 1.3, 1.4, 1.5, 1.7]
+revit = [0, 20, 40, 60, 80, 100]
 
 class Hero(object):
-    def __init__(self, name, atkSpeed, skillLevel, zerkLevel, revitLevel, procCD):
+    def __init__(self, name, atkSpeed, skillLevel):
         self.name = name
         self.atkSpeed = atkSpeed
         self.skillLevel = skillLevel
         self.hasAttacked = False
         self.celebrateStacks = []
-        self.zerkLevel = zerkLevel
+        self.zerkLevel = 0
         self.lastAttack = 0
         self.lastProc = -100000
-        self.energy = revit[revitLevel]
-        self.procCD = procCD
+        self.energy = 0
+        self.procCD = 0
         self.procCount = 0
-    
+        self.energyInc = 15
+
+    def setZerkLevel(self, zerkLevel):
+        self.zerkLevel = zerkLevel
+
+    def setRevitLevel(self, revitLevel):
+        self.energy = revit[revitLevel]
+
+    def setProcCD(self, procCD):
+        self.procCD = procCD
+
+    def setEnergyInc(self, energyInc):
+        self.energyInc = energyInc
+
     def hasAttacked(self):
         return self.hasAttacked
-    
+
     def setAttack(self, time):
         self.lastAttack = time
-    
+
     def getAttack(self):
         return self.lastAttack
-    
+
     def getAtkSpeed(self):
-        if len(self.celebrateStacks)>0:
-            tempSpeed = self.atkSpeed
-            for stack in self.celebrateStacks:
-                tempSpeed = tempSpeed / celebrate[stack[0]]
-            return ceil(tempSpeed/zerk[self.zerkLevel],200)
-        else:
-            return ceil(self.atkSpeed/zerk[self.zerkLevel], 200)
-    
+        tempSpeed = self.atkSpeed
+
+        # Celebrate
+        for stack in self.celebrateStacks:
+            tempSpeed = tempSpeed / celebrate[stack[0]]
+
+        totalBuff = zerk[self.zerkLevel]
+        return ceil(tempSpeed / totalBuff, 200)
+
     def addCelebrate(self, celebrateLevel, time):
         self.celebrateStacks.append([celebrateLevel, time])
-    
+
     def addEnergy(self, energy):
-        self.energy += energy
-        
+        self.energy = min(100, round(self.energy + energy, 2))
+
     def update(self, time):
         if self.hasAttacked == True:
             if self.lastAttack + self.getAtkSpeed() <= time:
                 self.hasAttacked = False
-        
+
         # Expire celebrate
         if len(self.celebrateStacks)>0:
             if self.celebrateStacks[0][1]<= time-8000:
                 self.celebrateStacks.pop(0)
-        
+
         if self.hasAttacked == False:
             self.hasAttacked = True
             self.lastAttack = time
             if self.energy >= 100 and self.lastProc + self.procCD <= time:
                 self.proc(time)
             print(self.name, "swing", self.energy, self.getAtkSpeed(), len(self.celebrateStacks))
-            if self.name[:5] == "Aries":
-                self.energy += 12
-            else:
-                self.energy += 15
-    
+            self.addEnergy(self.energyInc)
+
     def proc(self, time):
         self.energy = 0
         self.procCount += 1
@@ -79,18 +95,27 @@ class Hero(object):
             for hero in heroArray:
                 if hero.name[:5] != "Cupid":
                     hero.addEnergy(arrow[self.skillLevel])
-                
-   
-# Create your heroes here, folloing the format:
-# Hero("HeroName", AtkSpeed, SkillLevel, ZerkLevel, ReviteLevel, Skill CD)
-# Pumpkin Duke must have a name that starts with "PD" and Cupid must have a
-# name that starts with "Cupid".
-PD = Hero("PD", 1000, 1, 0, 4, 0)
-#Cupid = Hero("Cupid", 1200, 9, 0, 5, 6000)
+
+
+# Create your heroes here, following the format:
+#   hero = Hero("HeroName", AtkSpeed, SkillLevel)
+# After creating a hero, you can specify any of the following:
+#   hero.setZerkLevel(zerk level)
+#   hero.setRevitLevel(revit level)
+#   hero.setProcCD(proc CD in ms)
+#   hero.setEnergyInc(energy increment per hit) - default=15
+#
+#  A few heroes must have a name that starts with a specific prefix:
+#   Pumpkin Duke must start with "PD".
+#   Cupid must start with "Cupid".
+
+pd = Hero("PD", 1000, 10)
+pd.setZerkLevel(8)
+pd.setRevitLevel(5)
 
 # After you create your heroes, add the variable names to this array, then hit
 # the "run" button above
-heroArray = [PD]
+heroArray = [pd]
 
 # Output shows the time in 100ms incriments, it shows the energy *before* the
 # action taken (i.e. PD1 swwing 0 800.0 0) means it had 0 energy before that 
@@ -108,4 +133,3 @@ for i in range(0, 30000, 100):
 print("Total procs per hero:")
 for hero in heroArray:
     print(hero.name, hero.procCount)
-        
